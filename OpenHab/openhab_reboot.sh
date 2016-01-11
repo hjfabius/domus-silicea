a22a2f1763de9837b836d60ac1898e3b52734fd3
hjfabius <hjfabius@users.noreply.github.com>
2016-01-11 22:31:02 +0100

    Update openhab_reboot.sh

--- OpenHab/openhab_reboot.sh
+++ OpenHab/openhab_reboot.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo $(date + "%T") >> /opt/openhab/logs/openhab_stop.log
+
+date  >> /opt/openhab/logs/openhab_stop.log
 echo "openhab_reboot.sh - start" >> /opt/openhab/logs/openhab_stop.log
 
 echo "Stopping the service"  >> /opt/openhab/logs/openhab_stop.log
 sudo /etc/init.d/openhab stop
 
-echo "Uploading rrd4j files on google"  >> /opt/openhab/logs/openhab_stop.log
 cd /opt/openhab/etc/rrd4j/
+
+echo "Upload" >> /opt/openhab/etc/rrd4j/last_refresh.log
+date >> /opt/openhab/etc/rrd4j/last_refresh.log
+
+echo "Uploading rrd4j files on google"  >> /opt/openhab/logs/openhab_stop.log
 sudo touch *
-/usr/local/bin/gdrive push -no-prompt  >> /opt/openhab/logs/openhab_stop.log
+/usr/local/bin/gdrive push -no-prompt -force  >> /opt/openhab/logs/openhab_stop.log
 
 echo "reboot"   >> /opt/openhab/logs/openhab_stop.log
 sudo /sbin/reboot
