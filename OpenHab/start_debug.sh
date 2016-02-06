ef5c7fcdcd6960184072a6cfb78435ca76092f16
hjfabius <hjfabius@gmail.com>
2016-02-07 00:49:18 +0100

    Tentative SQLLite (not fully working

--- OpenHab/start_debug.sh
+++ OpenHab/start_debug.sh
@@ -12,21 +12,24 @@ HTTPS_PORT=8443
 # get path to equinox jar inside $eclipsehome folder
 cp=$(find $eclipsehome -name "org.eclipse.equinox.launcher_*.jar" | sort | tail -1);
 
-echo Launching the openHAB runtime...
-/usr/local/jdk1.8.0_65/bin/java \
+# debug options
+debug_opts="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n"
+
+echo Launching the openHAB runtime in debug mode...
+/usr/local/jdk1.8.0_65/bin/java  $debug_opts \
 	-Dgnu.io.rxtx.SerialPorts=/dev/ttyAMA0 \
     -Dosgi.clean=true \
 	-Declipse.ignoreApp=true \
-	-Dosgi.noShutdown=true  \
-	-Djetty.port=$HTTP_PORT  \
+	-Dosgi.noShutdown=true \
+	-Djetty.port=$HTTP_PORT \
 	-Djetty.port.ssl=$HTTPS_PORT \
-	-Djetty.home=.  \
-	-Dlogback.configurationFile=configurations/logback.xml \
-	-Dfelix.fileinstall.dir=addons 
+	-Djetty.home=. \
+	-Dlogback.configurationFile=configurations/logback_debug.xml \
+	-Dfelix.fileinstall.dir=addons \
 	-Dfelix.fileinstall.filter=.*\\.jar \
 	-Djava.library.path=lib \
-	-Djava.security.auth.login.config=./etc/login.conf \
 	-Dorg.quartz.properties=./etc/quartz.properties \
+	-Djava.security.auth.login.config=./etc/login.conf \
 	-Dequinox.ds.block_timeout=240000 \
 	-Dequinox.scr.waitTimeOnBlock=60000 \
 	-Dfelix.fileinstall.active.level=4 \
