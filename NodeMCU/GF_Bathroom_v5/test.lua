c1a776a3cf657012badb0192e5a1eec364c6d873
hjfabius <hjfabius@gmail.com>
2016-02-07 00:48:19 +0100

    NodeMCU v5

---
+++ NodeMCU/GF_Bathroom_v5/test.lua
@@ -0,0 +1,20 @@
+print ("domus-silicea - test.lua - Loading Modules")
+
+
+config         = require "config"   
+--configWifi 	   = require "configWifi"   
+DHT11          = require "DHT11" 
+--gasMeter       = require "gasMeter"
+--waterMeter     = require "waterMeter"
+--powerMeter     = require "powerMeter"
+--WIFI  		   = require "WIFI"
+--MQTT           = require "MQTT"
+--app            = require "app"   
+
+print ("domus-silicea - test.lua - Script Starting")
+--WIFI.start() 
+DHT11.update()
+
+print ("domus-silicea - test.lua - Script Ended")
+
+
