f3b6422adff82925d4b2f9df165ab273fd4f570c
hjfabius <hjfabius@gmail.com>
2016-02-25 00:08:21 +0100

    Openhab MySQL

---
+++ OpenHab/iPhoneEA.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+declare -a DEVICES
+hping3 -2 -c 10 -p 5353 -i u1 192.168.0.16 -q >/dev/null 2>&1 
+DEVICES=`arp -an | awk '{print $4}'`
+CHECK="AA:BB:CC:DD:EE:FF"
+if [[ ${DEVICES[*]} =~ $CHECK ]]
+then
+echo "Present"
+else
+echo "Absent"
+fi
--- OpenHab/start.sh
+++ OpenHab/start.sh
@@ -12,21 +12,24 @@ HTTPS_PORT=8443
 # get path to equinox jar inside $eclipsehome folder
 cp=$(find $eclipsehome -name "org.eclipse.equinox.launcher_*.jar" | sort | tail -1);
 
+
+
+
 echo Launching the openHAB runtime...
 /usr/local/jdk1.8.0_65/bin/java \
 	-Dgnu.io.rxtx.SerialPorts=/dev/ttyAMA0 \
     -Dosgi.clean=true \
 	-Declipse.ignoreApp=true \
-	-Dosgi.noShutdown=true  \
-	-Djetty.port=$HTTP_PORT  \
+	-Dosgi.noShutdown=true \
+	-Djetty.port=$HTTP_PORT \
 	-Djetty.port.ssl=$HTTPS_PORT \
-	-Djetty.home=.  \
+	-Djetty.home=. \
 	-Dlogback.configurationFile=configurations/logback.xml \
-	-Dfelix.fileinstall.dir=addons 
+	-Dfelix.fileinstall.dir=addons \
 	-Dfelix.fileinstall.filter=.*\\.jar \
 	-Djava.library.path=lib \
-	-Djava.security.auth.login.config=./etc/login.conf \
 	-Dorg.quartz.properties=./etc/quartz.properties \
+	-Djava.security.auth.login.config=./etc/login.conf \
 	-Dequinox.ds.block_timeout=240000 \
 	-Dequinox.scr.waitTimeOnBlock=60000 \
 	-Dfelix.fileinstall.active.level=4 \
