ac5afd4246d765d341a4fa77a608793287e6d4bf
hjfabius <hjfabius@gmail.com>
2016-03-07 01:35:52 +0100

    NodeMCU v9

---
+++ NodeMCU/FF_LivingRoom_v9/configWifi.lua
@@ -0,0 +1,13 @@
+-- Global Configuration File --
+local configWifi = {}
+
+	print ("domus-silicea - configWifi.lua - Script Starting") 
+	
+    configWifi.SSID = "HJ_HOME"
+    configWifi.PWD = "8082808280"
+
+    configWifi.IP  = ""
+    configWifi.MAC  = ""
+    
+return configWifi 
+
